8ffe3e26d67aedb0e3fc9f76750ba518a413487c
Arthur <aprendizadoarthur@gmail.com>
2025-03-05 09:55:38 -0300

    Criado arquivos para outras páginas e links de navegação

--- functions.php
+++ functions.php
@@ -12,29 +12,29 @@ function cabecalho() {
                             <ul class="navbar-nav text-center ml-auto">
                                 <hr class="d-md-none>
                                 <li class="nav-item text-center mx-2">
-                                    <a class="nav-link poppins-regular" rel="nofollow" href="">Argumentos</a>
+                                    <a class="nav-link poppins-regular" rel="nofollow" href="argumentos.php">Argumentos</a>
                                 </li>
                                 <li class="nav-item text-center mx-2">
-                                    <a class="nav-link poppins-regular" href="">Redações</a>
+                                    <a class="nav-link poppins-regular" href="redacoes.php">Redações</a>
                                 </li>
                                 <li class="nav-item text-center mx-2">
-                                    <a class="nav-link poppins-regular" rel="nofollow" href="">Repertórios</a>
+                                    <a class="nav-link poppins-regular" rel="nofollow" href="repertorios.php">Repertórios</a>
                                 </li>
                                 <li class="nav-item text-center mx-2">
-                                    <a class="nav-link poppins-regular" rel="nofollow" href="">Temas</a>
+                                    <a class="nav-link poppins-regular" rel="nofollow" href="temas.php">Temas</a>
                                 </li>
                                 <li class="nav-item text-center">
                                     <hr class="my-3">
-                                    <a class="nav-link d-md-none my-2 botao-principal poppins-semibold py-2" rel="nofollow" href=""><i class="fa-solid fa-list-check mr-2" style="color: #ffffff;"></i>Trilha de aprendizado</a>
+                                    <a class="nav-link d-md-none my-2 botao-principal poppins-semibold py-2" rel="nofollow" href="trilha.php"><i class="fa-solid fa-list-check mr-2" style="color: #ffffff;"></i>Trilha de aprendizado</a>
                                 </li>
                                 <li class="nav-item text-center">
-                                    <a class="nav-link d-md-none my-2 botao-principal poppins-semibold py-2" rel="nofollow" href=""><i class="fa-solid fa-file-pdf fa-lg mr-2" style="color: #ffffff;"></i>Conteúdo em PDF</a>
+                                    <a class="nav-link d-md-none my-2 botao-principal poppins-semibold py-2" rel="nofollow" href="pdf.php"><i class="fa-solid fa-file-pdf fa-lg mr-2" style="color: #ffffff;"></i>Conteúdo em PDF</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link d-md-none my-2 botao-atencao poppins-semibold py-2" rel="nofollow" href=""><i class="fa-solid fa-hand-holding-dollar fa-lg mr-2"></i>Doe para o projeto</a>
+                                    <a class="nav-link d-md-none my-2 botao-atencao poppins-semibold py-2" rel="nofollow" href="https://www.vakinha.com.br/?utm_source=google&utm_medium=cpc&utm_campaign=Vakinha_google_conversao_search_fundo_11_always-on_institucional_home-cpc&utm_content=120558729261&utm_term=vakinha&gad_source=1&gclid=CjwKCAiAiaC-BhBEEiwAjY99qLxNRAJTaXahJbcRTjgZAYMWKhH4W_wga5Zxv6NKnU_OnELKF2H26xoCNFsQAvD_BwE"><i class="fa-solid fa-hand-holding-dollar fa-lg mr-2"></i>Doe para o projeto</a>
                                 </li>
                                 <li class="nav-item ml-2">
-                                    <a class="nav-link d-none d-md-block botao-principal poppins-semibold py-2 px-3" rel="nofollow" href=""><i class="fa-solid fa-hand-holding-dollar fa-lg"></i></a>
+                                    <a class="nav-link d-none d-md-block botao-principal poppins-semibold py-2 px-3 aumentar" rel="nofollow" href="https://www.vakinha.com.br/?utm_source=google&utm_medium=cpc&utm_campaign=Vakinha_google_conversao_search_fundo_11_always-on_institucional_home-cpc&utm_content=120558729261&utm_term=vakinha&gad_source=1&gclid=CjwKCAiAiaC-BhBEEiwAjY99qLxNRAJTaXahJbcRTjgZAYMWKhH4W_wga5Zxv6NKnU_OnELKF2H26xoCNFsQAvD_BwE"><i class="fa-solid fa-hand-holding-dollar fa-lg"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -57,19 +57,19 @@ function rodape(){
                                         <span class="nav-link poppins-semibold preto" href="">Conteúdo</span>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Argumentos</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="argumentos.php">Argumentos</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Conteúdo em PDF</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="pdf.php">Conteúdo em PDF</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Redações</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="redacoes.php">Redações</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Repertórios</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="repertorios.php">Repertórios</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Temas</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="temas.php">Temas</a>
                                     </li>
                                 </ul>
                             </div>
@@ -80,13 +80,13 @@ function rodape(){
                                         <span class="nav-link poppins-semibold preto" href="">Projeto</span>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Doe para o projeto</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="https://www.vakinha.com.br/?utm_source=google&utm_medium=cpc&utm_campaign=Vakinha_google_conversao_search_fundo_11_always-on_institucional_home-cpc&utm_content=120558729261&utm_term=vakinha&gad_source=1&gclid=CjwKCAiAiaC-BhBEEiwAjY99qLxNRAJTaXahJbcRTjgZAYMWKhH4W_wga5Zxv6NKnU_OnELKF2H26xoCNFsQAvD_BwE">Doe para o projeto</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Privacidade</a>
+                                        <a class="nav-link poppins-regular" rel="nofollow" href="privacidade.php">Privacidade</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link poppins-regular" href="">Sobre</a>
+                                        <a class="nav-link poppins-regular"  rel="nofollow" href="sobre.php">Sobre</a>
                                     </li>
                                 </ul>
                             </div>
--- temas.php
+++ temas.php
@@ -11,9 +11,9 @@ include('functions.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!--TÍTULO/ÍCONE/DESCRIÇÃO DA PÁGINA/COR TEMA NAVEGADOR-->
-    <title>Redamil - Enriqueça a sua redação do ENEM!</title>
+    <title>Redamil - Temas</title>
     <link rel="icon" type="image/x-icon" href="images/assets/icon.ico">
-    <meta name="description" content="Redamil: Repertórios gratuitos para redação do ENEM, temas para praticar, argumentos e redações nota 1000 comentadas. Domine a escrita do ENEM agora!">
+    <meta name="description" content="Pratique redação com temas atuais para o ENEM e textos de apoio exclusivos. Prepare-se para a prova com confiança!">
     <meta name="theme-color" content="#3E3E42">
     
 <!--BOOTSTRAP CSS-->
@@ -39,36 +39,15 @@ include('functions.php');
 
         <!--ABERTURA MAIN-->
         <main class="mb-3">
-        <!--ABERTURA SESSÃO COM CTA E NAVEGAÇÃO CONTEÚDO-->
+        <!--ABERTURA SESSÃO-->
             <div class="container box">
-                <section id="cta-nav">
+                <section id="">
                     <div class="row d-flex p-2">
-                        <div class="col-12 col-lg-6 my-1 p-4 text-center cta-index d-flex align-items-center justify-content-center">
-                            <h1 class="d-block d-mb-none poppins-semibold text-light" style="font-size: 1.7rem">Domine a redação do ENEM com materiais completos e grátis!</h1>
-                            <h1 class="d-none d-mb-block poppins-semibold text-light">Domine a redação do ENEM com materiais completos e grátis!</h1>
                         
-                            <a href="trilha.php" class="d-block botao-atencao poppins-semibold mt-3 p-2 aumentar"><i class="fa-solid fa-list-check mr-2" style="color: #3E3E42"></i>Trilha de aprendizado</a>
-                        </div>
-                        <div class="col-12 col-lg-6 my-1 text-center d-flex">
-                            <div class="row">
-                                <div class="col-12 col-md-6 my-1 col-lg-12">
-                                    <a href="argumentos.php" class="d-block botao-principal poppins-semibold p-2 aumentar"><i class="fa-solid fa-quote-left fa-lg mr-2" style="color: #ffffff;"></i>Argumentos</a>
-                                </div>
-                                <div class="col-12 col-md-6 my-1 col-lg-12">
-                                    <a href="redacoes.php" class="d-block botao-principal poppins-semibold p-2 aumentar"><i class="fa-solid fa-file-invoice fa-lg mr-2" style="color: #ffffff;"></i>Redações</a>
-                                </div>
-                                <div class="col-12 col-md-6 my-1 col-lg-12">
-                                    <a href="repertorios.php" class="d-block botao-principal poppins-semibold p-2 aumentar"><i class="fa-solid fa-folder-open fa-lg mr-2" style="color: #ffffff;"></i>Repertórios</a>
-                                </div>
-                                <div class="col-12 col-md-6 my-1 col-lg-12">
-                                    <a href="temas.php" class="d-block botao-principal poppins-semibold p-2 aumentar"><i class="fa-solid fa-lightbulb fa-lg mr-2" style="color: #ffffff;"></i>Temas</a>
-                                </div>    
-                            </div>
-                        </div>
                     </div> 
                 </section>
             </div>
-         <!--FECHAMENTO SESSÃO COM CTA E NAVEGAÇÃO CONTEÚDO-->
+         <!--FECHAMENTO SESSÃO-->
         </main>
         <!--FECHAMENTO MAIN-->    
         
